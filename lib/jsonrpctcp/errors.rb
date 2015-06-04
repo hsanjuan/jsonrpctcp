c335a76ac2bd95e6bcf16d5418e906a4d4bc0e34
Hector Sanjuan <hector@convivencial.org>
2015-06-05 01:07:11 +0200

    add headers

--- lib/jsonrpctcp/errors.rb
+++ lib/jsonrpctcp/errors.rb
@@ -1,3 +1,20 @@
+# Copyright (C) 2015 Hector Sanjuan
+
+# This file is part of Jsonrpctcp.
+
+# Jsonrpctcp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Jsonrpctcp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Jsonrpctcp.  If not, see <http://www.gnu.org/licenses/>
+
 module Jsonrpctcp
 
   # A custom exception for the library
